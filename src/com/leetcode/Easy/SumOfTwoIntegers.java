65d903473a6e4629998c652ebaead7858038e184
Shankar Mattigatti <shankarmattigatti@gmail.com>
2019-05-16 07:47:18 +0530

    Added Solutions to 371. Sum of Two Integers

---
+++ src/com/leetcode/Easy/SumOfTwoIntegers.java
@@ -0,0 +1,13 @@
+package com.leetcode.Easy;
+
+// 371. Sum of Two Integers
+public class SumOfTwoIntegers {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+}
