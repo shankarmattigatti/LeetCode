361f5b430703a466cdb6dec323db88801f59948a
Shankar Mattigatti <shankarmattigatti@gmail.com>
2019-11-08 08:09:52 +0530

    Added Solutions to 190. Reverse Bits

---
+++ src/com/leetcode/Easy/ReverseBits.java
@@ -0,0 +1,9 @@
+package com.leetcode.Easy;
+
+// 190. Reverse Bits
+public class ReverseBits {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        return Integer.reverse(n);
+    }
+}
