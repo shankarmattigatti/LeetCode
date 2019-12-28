7051522114fdb85c4e050f3f19324e4aa82b8822
Shankar Mattigatti <shankarmattigatti@gmail.com>
2019-12-28 10:33:04 +0530

    Refactored Solution 485. Max Consecutive Ones

--- src/com/leetcode/Easy/MaxConsecutiveOnes.java
+++ src/com/leetcode/Easy/MaxConsecutiveOnes.java
@@ -5,10 +5,10 @@ public class MaxConsecutiveOnes {
 
     public int findMaxConsecutiveOnes(int[] nums) {
         int count = 0, maxCount = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 1) {
+        for (int num : nums) {
+            if (num == 1) {
                 count++;
-                maxCount = count > maxCount ? count : maxCount;
+                maxCount = Math.max(count, maxCount);
             } else {
                 count = 0;
             }
