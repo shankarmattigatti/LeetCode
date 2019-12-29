ede8c8ee0d47e7942587bdf95e3ea4d8d77685ec
Shankar Mattigatti <shankarmattigatti@gmail.com>
2019-12-29 14:50:16 +0530

    Refactored Solution 74. Search a 2D Matrix

--- src/com/leetcode/Medium/SearchA2DMatrix.java
+++ src/com/leetcode/Medium/SearchA2DMatrix.java
@@ -3,13 +3,13 @@ package com.leetcode.Medium;
 // 74. Search a 2D Matrix
 public class SearchA2DMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for (int i = 0; i < matrix.length; i++) {
-            int low = 0, high = matrix[i].length - 1;
+        for (int[] ints : matrix) {
+            int low = 0, high = ints.length - 1;
             while (low <= high) {
                 int mid = (low + high) / 2;
-                if (matrix[i][mid] == target) {
+                if (ints[mid] == target) {
                     return true;
-                } else if (target < matrix[i][mid]) {
+                } else if (target < ints[mid]) {
                     high = mid - 1;
                 } else {
                     low = mid + 1;
