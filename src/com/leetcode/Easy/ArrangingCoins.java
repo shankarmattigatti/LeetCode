683a963810f687470aed0f0f58d982d9aa79e0a7
Shankar Mattigatti <shankarmattigatti@gmail.com>
2020-07-10 19:06:44 +0530

    Added Solutions 441. Arranging Coins

---
+++ src/com/leetcode/Easy/ArrangingCoins.java
@@ -0,0 +1,19 @@
+package com.leetcode.Easy;
+
+// 441. Arranging Coins
+public class ArrangingCoins {
+    public int arrangeCoins(int n) {
+        if (n < 0)
+            return -1;
+        if (n == 0)
+            return 0;
+
+        int rows = 0;
+        long sum = 0;
+        while (sum <= n) {
+            sum += rows + 1;
+            rows++;
+        }
+        return rows - 1;
+    }
+}
