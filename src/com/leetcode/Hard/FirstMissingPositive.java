2e169dba2d77e65f2d9187523c3a74d1f66e3fcf
Shankar Mattigatti <shankarmattigatti@gmail.com>
2021-03-08 21:42:26 +0530

    Added Solution to 41. First Missing Positive

---
+++ src/com/leetcode/Hard/FirstMissingPositive.java
@@ -0,0 +1,31 @@
+package com.leetcode.Hard;
+
+// 41. First Missing Positive
+public class FirstMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+        if(nums.length == 0)
+            return 1;
+
+        int i = 0;
+        while(i < nums.length) {
+            if(nums[i] > 0 && nums[i] < nums.length && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            } else {
+                i++;
+            }
+        }
+
+        for(i = 0; i < nums.length; i++) {
+            if(nums[i] != i + 1)
+                return i + 1;
+        }
+
+        return nums.length + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
