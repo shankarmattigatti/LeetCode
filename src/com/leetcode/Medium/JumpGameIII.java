8fa52e3b4eb4dc1e5f780659a662a694e1c846f5
Shankar Mattigatti <shankarmattigatti@gmail.com>
2021-12-09 07:47:59 +0530

    Added Solution 1306. Jump Game III

---
+++ src/com/leetcode/Medium/JumpGameIII.java
@@ -0,0 +1,13 @@
+package com.leetcode.Medium;
+
+// 1306. Jump Game III
+public class JumpGameIII {
+    public boolean canReach(int[] arr, int start) {
+        if (start >= 0 && start < arr.length && arr[start] < arr.length) {
+            int jump = arr[start];
+            arr[start] += arr.length;
+            return jump == 0 || canReach(arr, start + jump) || canReach(arr, start - jump);
+        }
+        return false;
+    }
+}
