498db93c32281927bda1a21ff532c5dd598d5beb
Shankar Mattigatti <shankarmattigatti@gmail.com>
2022-03-13 08:10:57 +0530

    Added Solution 367. Valid Perfect Square

---
+++ src/com/leetcode/Easy/ValidPerfectSquare.java
@@ -0,0 +1,19 @@
+package com.leetcode.Easy;
+
+// 367. Valid Perfect Square
+public class ValidPerfectSquare {
+    public boolean isPerfectSquare(int num) {
+        long left = 1, right = num;
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            if (mid * mid == num)
+                return true;
+
+            if (mid * mid < num)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return false;
+    }
+}
