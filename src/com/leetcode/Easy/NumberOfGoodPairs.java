53b40a3f0a125a252a9093a8a15fd6bd12acae5e
Shankar Mattigatti <shankarmattigatti@gmail.com>
2022-05-07 09:42:14 +0530

    Added Solution 1512. Number of Good Pairs

---
+++ src/com/leetcode/Easy/NumberOfGoodPairs.java
@@ -0,0 +1,15 @@
+package com.leetcode.Easy;
+
+// 1512. Number of Good Pairs
+public class NumberOfGoodPairs {
+    public int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j] && i < j)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
