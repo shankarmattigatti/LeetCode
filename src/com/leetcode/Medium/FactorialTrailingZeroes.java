313e5b475c2ce2c3bcd8be588a59ba71dea18e0b
Shankar Mattigatti <shankarmattigatti@gmail.com>
2022-06-01 08:42:31 +0530

    Added Solution 172. Factorial Trailing Zeroes

---
+++ src/com/leetcode/Medium/FactorialTrailingZeroes.java
@@ -0,0 +1,13 @@
+package com.leetcode.Medium;
+
+// 172. Factorial Trailing Zeroes
+public class FactorialTrailingZeroes {
+    public int trailingZeroes(int n) {
+        int countZeros = 0;
+        while (n > 0) {
+            n /= 5;
+            countZeros += n;
+        }
+        return countZeros;
+    }
+}
