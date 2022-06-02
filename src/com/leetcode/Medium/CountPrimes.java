0a1ccd711a202606665870a1cd8c111c1691ab57
Shankar Mattigatti <shankarmattigatti@gmail.com>
2022-06-02 07:46:06 +0530

    Added Solution 204. Count Primes

---
+++ src/com/leetcode/Medium/CountPrimes.java
@@ -0,0 +1,23 @@
+package com.leetcode.Medium;
+
+// 204. Count Primes
+public class CountPrimes {
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public int countPrimes(int n) {
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime(i))
+                count++;
+        }
+        return count;
+    }
+}
