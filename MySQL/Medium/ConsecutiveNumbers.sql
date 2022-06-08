897c3d46fdd7cd72cab5d0682ca831bbc1fa0d4f
Shankar Mattigatti <shankarmattigatti@gmail.com>
2022-06-08 07:13:46 +0530

    Added Solution 180. Consecutive Numbers

---
+++ MySQL/Medium/ConsecutiveNumbers.sql
@@ -0,0 +1,6 @@
+-- 180. Consecutive Numbers
+SELECT DISTINCT l1.num AS ConsecutiveNums 
+FROM Logs l1 
+JOIN Logs l2 on l1.id=l2.id-1 
+JOIN Logs l3 on l1.id=l3.id-2
+WHERE l1.num=l2.num and l2.num=l3.num;
