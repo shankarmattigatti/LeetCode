da569e839a79147b4a21ad7ec1cd6f4f3d69d32b
Shankar Mattigatti <shankarmattigatti@gmail.com>
2022-06-10 16:48:32 +0530

    Added Solution 9. Palindrome Number

---
+++ JavaScript/Easy/PalindromeNumber.js
@@ -0,0 +1,13 @@
+// 9. Palindrome Number
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0) 
+        return false;
+	
+    const reversed = `${x}`.split('').reverse().join('');
+    return x == reversed;
+};
