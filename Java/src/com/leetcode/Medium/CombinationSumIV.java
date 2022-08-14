a1a5925260a022253732bb70883614bee2e7fcee
Shankar Mattigatti <shankarmattigatti@gmail.com>
2022-08-14 12:16:26 +1000

    Added Solution 377. Combination Sum IV

---
+++ Java/src/com/leetcode/Medium/CombinationSumIV.java
@@ -0,0 +1,26 @@
+package com.leetcode.Medium;
+
+import java.util.*;
+
+// 377. Combination Sum IV
+public class CombinationSumIV {
+    private final Map<Integer, Integer> map = new HashMap<>();
+
+    public int combinationSum4(int[] nums, int target) {
+        int count = 0;
+        if (nums == null || nums.length == 0 || target < 0)
+            return 0;
+
+        if (target == 0)
+            return 1;
+
+        if (map.containsKey(target))
+            return map.get(target);
+
+        for (int num : nums)
+            count += combinationSum4(nums, target - num);
+
+        map.put(target, count);
+        return count;
+    }
+}
