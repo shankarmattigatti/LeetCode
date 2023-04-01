c813f30b4e102e744cec34a9c60e2320acf70723
Shankar Mattigatti <shankarmattigatti@gmail.com>
2023-04-01 08:59:08 +0530

    Added Solution to 704. Binary Search

---
+++ JavaScript/Easy/BinarySearch.js
@@ -0,0 +1,22 @@
+/**
+ * 704. Binary Search
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let low = 0, high = nums.length - 1;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (nums[mid] == target) {
+      return mid;
+    } else if (target < nums[mid]) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return -1;
+};
