f1d58d3337cd4fc6dd9365321ea0da2e70b6cdec
Shankar Mattigatti <shankarmattigatti@gmail.com>
2023-04-14 21:17:38 +0530

    Added Solution to 516. Longest Palindromic Subsequence

---
+++ JavaScript/Medium/LongestPalindromicSubsequence.js
@@ -0,0 +1,26 @@
+/**
+ * 516. Longest Palindromic Subsequence
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindromeSubseq = function (s) {
+  const n = s.length;
+  const dp = Array(n).fill().map(() => Array(n).fill(0));
+
+  for (let i = 0; i < n; i++) {
+    dp[i][i] = 1;
+  }
+
+  for (let len = 2; len <= n; len++) {
+    for (let i = 0; i <= n - len; i++) {
+      const j = i + len - 1;
+      if (s[i] === s[j]) {
+        dp[i][j] = 2 + dp[i + 1][j - 1];
+      } else {
+        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  return dp[0][n - 1];
+};
