bf5e3519e5a8b9389b6e181d96e01b294fe68914
Shankar Mattigatti <shankarmattigatti@gmail.com>
2024-07-03 08:50:13 +0530

    Added Solution to 1509. Minimum Difference Between Largest and Smallest Value in Three Moves

---
+++ JavaScript/Medium/MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.js
@@ -0,0 +1,19 @@
+/**
+ * 1509. Minimum Difference Between Largest and Smallest Value in Three Moves
+ * @param {number[]} nums
+ * @return {number}
+ */
+const minDifference = function (nums) {
+  if (nums.length <= 4) return 0;
+
+  nums.sort((a, b) => a - b);
+  let n = nums.length;
+  let minDiff = Infinity;
+
+  minDiff = Math.min(minDiff, nums[n - 1] - nums[3]);
+  minDiff = Math.min(minDiff, nums[n - 2] - nums[2]);
+  minDiff = Math.min(minDiff, nums[n - 3] - nums[1]);
+  minDiff = Math.min(minDiff, nums[n - 4] - nums[0]);
+
+  return minDiff;
+};
